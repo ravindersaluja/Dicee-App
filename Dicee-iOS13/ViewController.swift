a8aa997abbd57101381cefdbf0da760f903ccaed
Ravinder Saluja <ravz.saluja@yahoo.com>
2020-04-17 00:32:09 +0530

    Change IB

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//    IBOutlet helps create a link b/w UI and code.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        Who.What = Value
+//        = Image Literal(Lets you select an image from the image assets)
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView1.alpha = 0.8
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView2.alpha = 0.8
     }
-
-
+//IBOutlet is from code to design
+//IBAction is from design to code
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = diceImageView1.image
+        
+    }
+    
 }
 
